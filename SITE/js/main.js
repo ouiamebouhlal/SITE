8c19fb0d2b26301733e3bb28bc83887489d2978c
ouiame bouhlal <ouiamebouhlal@gmail.com>
2025-12-27 18:20:18 +0100

    final header and navbar fix

--- SITE/js/main.js
+++ SITE/js/main.js
@@ -1,6 +1,6 @@
-// navbar function
 $(document).ready(function(){
     $('.fa-bars').click(function(){
-        $(this).toggleClass('fa-times');
+        $(this).toggleClass('fa-times'); // icon changes
+        $('.navbar').slideToggle();      // show/hide navbar
     });
 });
